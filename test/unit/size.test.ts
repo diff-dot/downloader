4ae16a8614d158a2d9e4dc355b1cd39bde005685
Hyogeun Wang <w@endless9.com>
2020-11-19 05:24:41 +0900

    주석 추가

--- test/unit/size.test.ts
+++ test/unit/size.test.ts
@@ -7,11 +7,16 @@ const testImagePath = 'https://yt3.ggpht.com/a-/AOh14GiwWzg6CLhEJU0bbBp6vfHPyJew
 describe('download', async () => {
   it('get image size of remote file', async () => {
     const downloader = new Downloader();
+
+    // 원격지 파일 크기 확인
     const size = await downloader.size(testImagePath);
     expect(size).to.be.eq(36587);
 
+    // 다운로드 받은 파일의 크기 확인
     const tmpPath = await downloader.download(testImagePath);
     const tmpFileStat = await promisify(fs.stat)(tmpPath);
+
+    // 같아야 하느니라
     expect(size).to.be.eq(tmpFileStat.size);
   });
 });
